5e7d7471c527e8a002c2c1e45e511d57d9796b38
Mathew Cibi <mcib088@aucklanduni.ac.nz>
2025-03-12 14:44:52 +1300

    Updated GMAIL API

--- src/main/java/me/protonplus/lumin/util/google/GoogleGmailAPI.java
+++ src/main/java/me/protonplus/lumin/util/google/GoogleGmailAPI.java
@@ -3,12 +3,12 @@ package me.protonplus.lumin.util.google;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.services.gmail.Gmail;
-import com.google.api.services.gmail.model.Label;
-import com.google.api.services.gmail.model.ListLabelsResponse;
+import com.google.api.services.gmail.model.ListMessagesResponse;
+import com.google.api.services.gmail.model.Message;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
-import java.util.List;
+import java.util.Arrays;
 
 import static me.protonplus.lumin.Lumin.APPLICATION_NAME;
 import static me.protonplus.lumin.util.google.GoogleCommon.JSON_FACTORY;
@@ -17,27 +17,26 @@ import static me.protonplus.lumin.util.google.GoogleCommon.getCredentials;
 public class GoogleGmailAPI {
 
     private static NetHttpTransport HTTP_TRANSPORT;
+    private final Gmail SERVICE;
 
     public GoogleGmailAPI() throws GeneralSecurityException, IOException {
         HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        SERVICE = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                .setApplicationName(APPLICATION_NAME)
+                .build();
     }
 
     public void getLatestEmail() throws GeneralSecurityException, IOException {
-        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-        Gmail service = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-                .setApplicationName(APPLICATION_NAME)
-                .build();
+        // Get the latest email:
+        // - Set max results to one.
+        // - Get emails from inbox.
+        // - Returns a JSON response with message ids.
+        ListMessagesResponse messagesResponse = SERVICE.users().messages().list("me").
+                setMaxResults(1L)
+                .setLabelIds(Arrays.asList("INBOX")).execute();
+        String id = messagesResponse.getMessages().get(0).getId();
 
-        String user = "me";
-        ListLabelsResponse listResponse = service.users().labels().list(user).execute();
-        List<Label> labels = listResponse.getLabels();
-        if (labels.isEmpty()) {
-            System.out.println("No labels found.");
-        } else {
-            System.out.println("Labels:");
-            for (Label label : labels) {
-                System.out.printf("- %s\n", label.getName());
-            }
-        }
+        Message messageResponse = SERVICE.users().messages().get("me", id).execute();
+        System.out.println(messageResponse.getSnippet());
     }
 }
